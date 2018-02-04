55ef16671eb381eccd98669e49f095eedf24cb4a
fmrsabino <fredericomsabino@gmail.com>
2018-02-04 17:34:28 +0100

    Use arrays along the turn as index

--- tictactoe.sol
+++ tictactoe.sol
@@ -1,71 +1,55 @@
 pragma solidity ^0.4.18;
 
 contract TicTacToe {
-  address player1;
-  address player2;
-  //0 for Player 1 // 1 for Player 2
-  uint8 turn;
-
-  bool missingDeposit = true;
-  uint16 player1Plays = 0x0;
-  uint16 player2Plays = 0x0;
+  address[2] players;
+  uint16[2] moves;
   
+  // 0 for Player1 // 1 for Player2
+  uint8 turn = 2;
   uint16[] winnings = [0x49, 0x92, 0x124, 0x7, 0x38, 0x1C0, 0x111, 0x54];
   
-  //Who creates the game is always Player1 
+  // Who creates the game is always Player0 
   function TicTacToe(address otherPlayer) public payable {
       if (msg.value == 1 ether) { //deposit made
-        player1 = msg.sender;
-        player2 = otherPlayer;
+        players[0] = msg.sender;
+        players[1] = otherPlayer;
       }
       //TODO: do not create game if deposit is not made
   }
   
   function deposit() public payable {
-      if (!missingDeposit) return;
-      if (msg.value != 1 ether && msg.sender != player2) return;
-      missingDeposit = false;
+      if (msg.value != 1 ether && msg.sender != players[1]) return;
       //TODO: pick starting player
+      turn = 0;
   }
   
   function play(uint8 position) public {
-      uint16 plays;
-      if (msg.sender == player1 && turn == 0) {
-          plays = player1Plays;
-      } else if (msg.sender == player2 && turn == 1) {
-          plays = player2Plays;
-      } else {
-          return;
-      }
+      if (turn == 2) return; //Game not running
+      if (msg.sender != players[turn]) return;
       
       uint16 move = uint16(0x1) << position;
       //Check if the other player already made this move
-      if (msg.sender == player1 && (player2Plays & move) == move || 
-          msg.sender == player2 && (player1Plays & move) == move) {
-          return;
-      }
+      if (moves[(turn + 1) % 2] & move == move) return;
       
-       // Set the play
-      if (msg.sender == player1) {
-          player1Plays |= move;
+      // Set the play
+      moves[turn] |= move;
+      
+      checkWinner();
+      
+      if ((moves[0] | moves[1]) == 0x1FF) { // tie
+          turn = 2; // end game
       } else {
-          player2Plays |= move;
+          turn = (turn + 1) % 2;
       }
-      
-      // Check the winner
+  }
+  
+  function checkWinner() private {
       for (uint8 i = 0; i < winnings.length; i++) {
-          if (winnings[i] & plays == winnings[i]) {
+          if (winnings[i] & moves[turn] == winnings[i]) {
               //Winner 
               turn = 2; //End game
               break;
           } 
-      }
-      
-      // Check tie
-      if ((player1Plays | player2Plays) == 0x1FF) {
-          turn = 2; // end game
-      } else {
-          turn = (turn + 1) % 2;
       }
   }
 }
