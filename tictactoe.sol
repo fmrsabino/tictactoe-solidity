45463a481be2f767f18c05710e1ce9857e012907
fmrsabino <fredericomsabino@gmail.com>
2018-02-04 18:20:18 +0100

    Add end game. Allow for partial deposits.

--- tictactoe.sol
+++ tictactoe.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.18;
 contract TicTacToe {
   address[2] players;
   uint16[2] moves;
+  uint256[2] deposits;
   
   // 0 for Player1 // 1 for Player2
   uint8 turn = 2;
@@ -10,17 +11,22 @@ contract TicTacToe {
   
   // Who creates the game is always Player0 
   function TicTacToe(address otherPlayer) public payable {
-      if (msg.value == 1 ether) { //deposit made
-        players[0] = msg.sender;
-        players[1] = otherPlayer;
-      }
-      //TODO: do not create game if deposit is not made
+      players[0] = msg.sender;
+      players[1] = otherPlayer;
+      deposit();
   }
   
   function deposit() public payable {
-      if (msg.value != 1 ether && msg.sender != players[1]) return;
-      //TODO: pick starting player
-      turn = 0;
+      if (msg.sender == players[0]) {
+          deposits[0] += msg.value;
+      } else if (msg.sender == players[1]) {
+          deposits[1] += msg.value;
+      }
+      
+      if (deposits[0] >= 1 ether && deposits[1] >= 1 ether) {
+          //TODO: pick starting player
+          turn = 0;
+      }
   }
   
   function play(uint8 position) public {
@@ -37,7 +43,9 @@ contract TicTacToe {
       checkWinner();
       
       if ((moves[0] | moves[1]) == 0x1FF) { // tie
-          turn = 2; // end game
+          endGame();
+          players[0].transfer(1 ether);
+          players[1].transfer(1 ether);
       } else {
           turn = (turn + 1) % 2;
       }
@@ -47,9 +55,19 @@ contract TicTacToe {
       for (uint8 i = 0; i < winnings.length; i++) {
           if (winnings[i] & moves[turn] == winnings[i]) {
               //Winner 
-              turn = 2; //End game
+              endGame();
+              players[turn].transfer(2 ether);
               break;
           } 
       }
+  }
+  
+  function endGame() private {
+      turn = 2;
+      moves[0] = 0;
+      moves[1] = 0;
+      // If the user transfered more than 1 ether we shouldn't reset the deposit to zero
+      deposits[0] -= 1 ether;
+      deposits[1] -= 1 ether;
   }
 }
